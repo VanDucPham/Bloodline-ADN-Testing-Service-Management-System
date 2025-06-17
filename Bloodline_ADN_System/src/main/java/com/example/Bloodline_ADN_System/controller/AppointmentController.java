b8941174ce8ee0ac033423b07da039a0a30b6d44
Phucdat25 <datnpse182125@fpt.edu.vn>
2025-06-17 16:48:51 +0700

    Validation tạo appointment. User lấy appointment. Manager xem tất cả appointment

--- Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/AppointmentController.java
+++ Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/AppointmentController.java
@@ -1,14 +1,15 @@
 package com.example.Bloodline_ADN_System.controller;
 
-import com.example.Bloodline_ADN_System.Entity.Appointment;
 import com.example.Bloodline_ADN_System.dto.AppointmentDTO;
 import com.example.Bloodline_ADN_System.dto.AppointmentResponse;
 import com.example.Bloodline_ADN_System.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/appointment")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
@@ -17,9 +18,27 @@ public class AppointmentController {
         this.appointmentService = appointmentService;
     }
 
-    @PostMapping("/appointment")
+    @PostMapping()
     public ResponseEntity<AppointmentResponse<AppointmentDTO>> createAppointment(@RequestBody AppointmentDTO dto) {
         AppointmentResponse<AppointmentDTO> response = appointmentService.createAppointment(dto);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public List<AppointmentDTO> getAllAppointment() {
+        List<AppointmentDTO> response = appointmentService.getAllAppointment();
+        return response;
+    }
+
+    @GetMapping("/{id}")
+    public List<AppointmentDTO> getAppointmentByUserId(@PathVariable Long id) {
+        List<AppointmentDTO> response = appointmentService.getAppointmentByUserId(id);
+        return response;
+    }
+
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity<?> cancelAppointment(@PathVariable Long id) {
+        appointmentService.cancelAppointment(id);
+        return ResponseEntity.ok("Hủy lịch hẹn thành công.");
     }
 }
