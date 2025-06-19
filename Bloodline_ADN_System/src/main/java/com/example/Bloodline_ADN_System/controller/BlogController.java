66fa4d6e581443377e39df97051bbf7a3be9dcbc
Haineee47 <phanhai0407@gmail.com>
2025-06-19 16:00:18 +0700

    chinh sua API /api/blog -> api/auth

--- Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/BlogController.java
+++ Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/BlogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/blogs")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class BlogController {
 
