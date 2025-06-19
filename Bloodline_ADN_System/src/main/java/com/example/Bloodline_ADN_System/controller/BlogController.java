d2d26854514479fffeb1223f4a201d1f8c66282c
Haineee47 <phanhai0407@gmail.com>
2025-06-19 16:21:40 +0700

    chinh sua API /api/blog -> api/admin

--- Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/BlogController.java
+++ Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/controller/BlogController.java
@@ -10,40 +10,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class BlogController {
 
     private final BlogService blogService;
 
     // ✅ Admin
-    @PostMapping("/admin/create")
+    @PostMapping("/create")
     public ResponseEntity<BlogResponseDTO> create(@RequestBody BlogRequestDTO dto) {
         return ResponseEntity.ok(blogService.createBlog(dto));
     }
 
-    @PutMapping("/admin/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<BlogResponseDTO> update(@PathVariable Long id, @RequestBody BlogRequestDTO dto) {
         return ResponseEntity.ok(blogService.updateBlog(id, dto));
     }
 
-    @DeleteMapping("/admin/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         blogService.deleteBlog(id);
         return ResponseEntity.ok("Xóa blog thành công");
     }
 
-    @GetMapping("/admin/all")
+    @GetMapping("/all")
     public ResponseEntity<List<BlogResponseDTO>> getAll() {
         return ResponseEntity.ok(blogService.getAllBlogs());
     }
 
-    @GetMapping("/admin/search")
+    @GetMapping("/search")
     public ResponseEntity<List<BlogResponseDTO>> search(@RequestParam String keyword) {
         return ResponseEntity.ok(blogService.searchBlogs(keyword));
     }
 
-    // ✅ Người dùng
+ /*   // ✅ Người dùng
     @GetMapping("/public")
     public ResponseEntity<List<BlogResponseDTO>> getPublishedBlogs() {
         return ResponseEntity.ok(blogService.getPublishedBlogs());
@@ -52,5 +52,5 @@ public class BlogController {
     @GetMapping("/public/{id}")
     public ResponseEntity<BlogResponseDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(blogService.getBlogById(id));
-    }
+    }*/
 }
