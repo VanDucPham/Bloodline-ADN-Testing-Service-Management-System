64593369ceb850cf565c75be1f6172f26ee57b30
VanDucPham <ducpvse181771@fpt.edu.vn>
2025-07-12 18:53:53 +0700

    nnn

--- pe-hsf302-trialexam-ducpvse181771/src/main/java/org/example/pehsf302trialexamducpvse181771/su25/ducpvse181771/repository/DirectorRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.pehsf302trialexamducpvse181771.su25.ducpvse181771.repository;
-
-import org.example.pehsf302trialexamducpvse181771.su25.ducpvse181771.pojo.Directors;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DirectorRepository extends JpaRepository<Directors, Integer> {
-
-}
--- pe-hsf302-trialexam-ducpvse181771/src/main/java/org/example/pehsf302trialexamducpvse181771/su25/ducpvse181771/repository/MoviesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.pehsf302trialexamducpvse181771.su25.ducpvse181771.repository;
-
-import org.example.pehsf302trialexamducpvse181771.su25.ducpvse181771.pojo.Movies;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoviesRepository extends JpaRepository<Movies, Integer> {
-}
