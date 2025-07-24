8bd025a016f249d3dcb3b83da33d1b63f90c2ddb
VanDucPham <ducpvse181771@fpt.edu.vn>
2025-07-24 21:19:10 +0700

    mmm

--- Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/config/WebConfig.java
+++ Bloodline_ADN_System/src/main/java/com/example/Bloodline_ADN_System/config/WebConfig.java
@@ -1,9 +1,9 @@
 package com.example.Bloodline_ADN_System.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,7 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
         // Map tất cả đường dẫn /upload/** tới thư mục gốc trên ổ cứng
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:Bloodline_ADN_System/upload/");
-        // nếu thư mục là ./upload trong thư mục project
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://bloodline-adn-testing-serv-git-dbd881-vanducs-projects-0cf321d4.vercel.app",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
-
